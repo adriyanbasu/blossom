331580e5f015210466f961db4743548a98c6d6a5
Prasenjit Basu <144541917+adriyanbasu@users.noreply.github.com>
2024-02-25 18:48:30 +0000

    commit1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
+enum Errors {}
+
+enum TokenType {
+    SemiColon,
+}
+
+struct Token {
+    pub ty: TokenType,
+    pub value: String,
+}
+
+fn tokenize(code: &str) -> Result<Vec<Token>, Errors> {
+    let tokens: Vec<Token> = Vec::new();
+    let current_token: Option<Token> = None;
+    for c in code.chars() {
+        if c == ';' {
+            if let Some(tok) = current_token {
+                tokens.push(tok);
+            }
+            tokens.push(Token {
+                ty: TokenType::SemiColon,
+                value: String::from(";"),
+            });
+
+            current_token = None;
+        } else if c == '=' {
+            if let Some(tok) = current_token {
+                tokens.push(tok)
+            }
+        }
+    }
+    Ok(tokens)
+}
+
 fn main() {
-    println!("Hello CodeSandbox!");
+    let tokens = tokenize("x = 2;").unwrap();
+
+    println!("{:?}", tokens)
 }
